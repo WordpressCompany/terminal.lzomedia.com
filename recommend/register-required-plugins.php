d2da8ed3f3a8d95e1a91a5e424c33c696236953f
Stefan <bogdan.izdrail@gmail.com>
2022-05-12 15:44:30 +0100

    renamed the slugs and menu links

--- recommend/register-required-plugins.php
+++ recommend/register-required-plugins.php
@@ -51,9 +51,9 @@ function questions_register_required_plugins() {
 	 * Only uncomment the strings in the config array if you want to customize the strings.
 	 */
 	$config = array(
-		'id'           => 'questions',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'id'           => 'terminal',                 // Unique ID for hashing notices for multiple instances of TGMPA.
 		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'menu'         => 'terminal-install-plugins', // Menu slug.
 		'parent_slug'  => 'themes.php',            // Parent menu slug.
 		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
 		'has_notices'  => true,                    // Show admin notices or not.
