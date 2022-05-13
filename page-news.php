e86f18375112d35b8a1a754e5b00e279a691b2eb
Stefan <bogdan.izdrail@gmail.com>
2022-05-13 12:21:34 +0100

    Replace the ol with divs

--- page-news.php
+++ page-news.php
@@ -9,27 +9,40 @@
 <section class="container">
     <div class="row">
         <div class="col-lg-12">
-            <ol class="terminal-toc">
 				<?php
 				$paged    = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
 				$args     = [
 					'post_type'      => 'post',
-					'posts_per_page' => 20,
+					'posts_per_page' => 45,
 					'paged'          => $paged,
 				];
 				$wp_query = new WP_Query( $args );
 
 				while ( have_posts() ) : the_post(); ?>
-                    <li>
-                        <a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>">
-                            <?php the_title(); ?>
-                        </a>
-                    </li>
+
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-10 col-md-10 col-lg-10">
+                            <div class="box">
+                                <a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>">
+		                            <?php the_title(); ?>
+                                </a>
+                            </div>
+                        </div>
+
+
+                        <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
+                            <div class="box">
+								<?php the_time('F j, Y') ?>
+                            </div>
+                        </div>
+
+
+                    </div>
+
 				<?php endwhile; ?>
 
 
-            </ol>
-            <!-- then the pagination links -->
+            </ol            <!-- then the pagination links -->
             <div class="row center-xs" style="margin-top:4rem;margin-bottom: 4rem">
                 <div class="col-xs-6">
                     <div class="box pagination btn btn-primary">
