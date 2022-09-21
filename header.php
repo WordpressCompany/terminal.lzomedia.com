931bfc55d3c36770694d77030a905e8cd927cb50
Stefan <bogdan.izdrail@gmail.com>
2022-09-21 11:55:48 +0100

    Fix seo issue header

--- header.php
+++ header.php
@@ -3,16 +3,21 @@
 <head>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <meta name="theme-color" content="#ffffff"/>
-    <meta charset="<?php bloginfo('charset'); ?>"/>
-    <title>Stefan Izdrail - Senior Backend Developer</title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width" />
+    <title><?php wp_title( '|', true, 'right' ); ?> <?php echo bloginfo('name');?></title>
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
+    <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+    <!--[if lt IE 9]>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js?ver=3.7.0" type="text/javascript"></script>
+    <![endif]-->
     <?php wp_head(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <link rel="stylesheet" href="https://unpkg.com/terminal.css@0.7.2/dist/terminal.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css"
           type="text/css">
-<!--    <link href="https://fonts.googleapis.com/css2?family=Oldenburg&display=swap" rel="stylesheet">-->
-
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 
 </head>
