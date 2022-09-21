17dd8726d5efaeb4e6a9b790efeeedae5b90fee1
Stefan <bogdan.izdrail@gmail.com>
2022-09-21 12:03:10 +0100

    Added more design fixes

--- page-news.php
+++ page-news.php
@@ -9,6 +9,7 @@
 <section class="container">
     <div class="row">
         <div class="col-lg-12">
+            <ol class="terminal-toc">
 				<?php
 				$paged    = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
 				$args     = [
@@ -19,37 +20,24 @@
 				$wp_query = new WP_Query( $args );
 
 				while ( have_posts() ) : the_post(); ?>
-
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-10 col-md-10 col-lg-10">
-                            <div class="box">
-                                <a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>">
-		                            <?php the_title(); ?>
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                            <div class="box">
-								<?php the_time('F j, Y') ?>
-                            </div>
-                        </div>
-
-
-                    </div>
-
+                    <li>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </li>
 				<?php endwhile; ?>
 
 
             </ol            <!-- then the pagination links -->
             <div class="row center-xs" style="margin-top:4rem;margin-bottom: 4rem">
+                <?php if ( ! empty(get_previous_posts_link("Newer posts &rarr;"))):?>
                 <div class="col-xs-6">
                     <div class="box pagination btn btn-primary">
 
 						<?php next_posts_link( '&larr; Older posts' ); ?>
                     </div>
                 </div>
+                <?php endif; ?>
 
 
                 <?php if ( ! empty(get_previous_posts_link("Newer posts &rarr;"))):?>
