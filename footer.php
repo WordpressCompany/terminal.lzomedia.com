7691c8edbd6b96dbee503e3f7d51ef92625e2e8c
Stefan <bogdan.izdrail@gmail.com>
2022-09-24 09:35:48 +0100

    Added the analytics tracking code

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@ $domain =  str_replace($protocols, '', get_bloginfo('wpurl'));
 </main>
 
 <!-- This is the area where is loaded the first time the page is loaded -->
-<div id="editor" class="terminal shell" style="margin-top: 4em;">
+<div id="editor" class="terminal shell" style="margin-top: 4em; width: 75vw">
     <code data-term-input>Connecting to <?php echo $domain;?>...</code>
     <code data-term-input>Connected...</code>
     <code data-term-input>Page load complete</code>
@@ -23,5 +23,14 @@ $domain =  str_replace($protocols, '', get_bloginfo('wpurl'));
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/highlight.min.js" async defer></script>
 <script src="<?php echo get_template_directory_uri(); ?>/dist/app.js"></script>
+<!-- Google Lzo Media tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VSK9XHKSDH"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-VSK9XHKSDH');
+</script>
 </body>
 </html>
