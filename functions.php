d3e39b423cc626a302f212f787be02da651cabf8
Stefan <bogdan.izdrail@gmail.com>
2023-01-16 18:42:53 +0000

    Created a new post type for the projects

--- functions.php
+++ functions.php
@@ -45,3 +45,61 @@ if ( ! is_admin() ) {
 
 }
 
+//register projects
+// Register Custom Post Type
+function create_projects_post() {
+
+    $labels = array(
+        'name'                  => _x( 'Projects', 'Post Type General Name', 'terminal' ),
+        'singular_name'         => _x( 'Project', 'Post Type Singular Name', 'terminal' ),
+        'menu_name'             => __( 'Project', 'terminal' ),
+        'name_admin_bar'        => __( 'Project Type', 'terminal' ),
+        'archives'              => __( 'Project Archives', 'terminal' ),
+        'attributes'            => __( 'Project Attributes', 'terminal' ),
+        'parent_item_colon'     => __( 'Project Item:', 'terminal' ),
+        'all_items'             => __( 'All Items', 'terminal' ),
+        'add_new_item'          => __( 'Add New Item', 'terminal' ),
+        'add_new'               => __( 'Add New', 'terminal' ),
+        'new_item'              => __( 'New Item', 'terminal' ),
+        'edit_item'             => __( 'Edit Item', 'terminal' ),
+        'update_item'           => __( 'Update Item', 'terminal' ),
+        'view_item'             => __( 'View Item', 'terminal' ),
+        'view_items'            => __( 'View Items', 'terminal' ),
+        'search_items'          => __( 'Search Item', 'terminal' ),
+        'not_found'             => __( 'Not found', 'terminal' ),
+        'not_found_in_trash'    => __( 'Not found in Trash', 'terminal' ),
+        'featured_image'        => __( 'Featured Image', 'terminal' ),
+        'set_featured_image'    => __( 'Set featured image', 'terminal' ),
+        'remove_featured_image' => __( 'Remove featured image', 'terminal' ),
+        'use_featured_image'    => __( 'Use as featured image', 'terminal' ),
+        'insert_into_item'      => __( 'Insert into item', 'terminal' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this item', 'terminal' ),
+        'items_list'            => __( 'Items list', 'terminal' ),
+        'items_list_navigation' => __( 'Items list navigation', 'terminal' ),
+        'filter_items_list'     => __( 'Filter items list', 'terminal' ),
+    );
+    $args = array(
+        'label'                 => __( 'Project', 'terminal' ),
+        'description'           => __( 'This is a custom post type to be used for projects', 'terminal' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'editor' ),
+        'taxonomies'            => array( 'category', 'post_tag' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'page',
+        'show_in_rest'          => false,
+        'rest_base'             => 'projects',
+    );
+    register_post_type( 'projects', $args );
+
+}
+add_action( 'init', 'create_projects_post', 0 );
